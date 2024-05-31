6006fccd435d86253619714855503b7707d38a1f
Richard Burman <richard.burman@harperjames.co.uk>
2024-05-31 12:47:01 +0100

    Add assignment tasks, creating reverse and sort pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,12 @@ import { AppComponent } from './app.component';
 import { ShortenPipe } from './shorten.pipe';
 import { FilterPipe } from './filter.pipe';
 import { ReversePipe } from './reverse.pipe';
+import { SortPipe } from './sort.pipe';
 
 @NgModule({
   declarations: [
     AppComponent, 
-    ShortenPipe, FilterPipe, ReversePipe
+    ShortenPipe, FilterPipe, ReversePipe, SortPipe
   ],
   imports: [
     BrowserModule,
